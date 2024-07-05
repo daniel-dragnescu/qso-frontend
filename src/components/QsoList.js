3ed52fd2a59eaa00a64e05c7665cfad5041cb8f9
Dragnescu Daniel <dragnescu_daniel12@yahoo.com>
2024-07-05 15:37:06 +0100

    Added message when you don't have a qso logged.

--- src/components/QsoList.js
+++ src/components/QsoList.js
@@ -32,38 +32,46 @@ const QsoList = ({ qsoList, searchTerm, loading, onEdit, onDelete }) => {
 
   return (
     <section className="qso-list">
-      <ul>
-        {filteredQsoList.map((qso, index) => (
-          <li key={index} className="qso-list-item">
-            <div className="qso-item">
-              <strong>Callsign:&nbsp;</strong>
-              {qso.callsign}
-            </div>
-            <div className="qso-item">
-              <strong>RST Received:&nbsp;</strong>
-              {qso.rst_received}
-            </div>
-            <div className="qso-item">
-              <strong>RST Sent:&nbsp;</strong>
-              {qso.rst_sent}
-            </div>
-            <div className="qso-item">
-              <strong>Operator:&nbsp;</strong>
-              {qso.op}
-            </div>
-            <div className="qso-item">
-              <strong>Location:&nbsp;</strong>
-              {qso.qth}
-            </div>
-            <div className="qso-item">
-              <strong>Comments:&nbsp;</strong>
-              {qso.comments}
-            </div>
-            <button className="edit-qso-button" onClick={() => onEdit(qso)}>Edit</button>
-            <button className="delete-qso-button" onClick={() => onDelete(qso)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      {qsoList.length === 0 && (
+        <h3>There are no Logged QSOs yet.</h3>
+      )}
+      {filteredQsoList.length === 0 && searchTerm && (
+        <h3></h3>
+      )}
+      {filteredQsoList.length > 0 && (
+        <ul>
+          {filteredQsoList.map((qso, index) => (
+            <li key={index} className="qso-list-item">
+              <div className="qso-item">
+                <strong>Callsign:&nbsp;</strong>
+                {qso.callsign}
+              </div>
+              <div className="qso-item">
+                <strong>RST Received:&nbsp;</strong>
+                {qso.rst_received}
+              </div>
+              <div className="qso-item">
+                <strong>RST Sent:&nbsp;</strong>
+                {qso.rst_sent}
+              </div>
+              <div className="qso-item">
+                <strong>Operator:&nbsp;</strong>
+                {qso.op}
+              </div>
+              <div className="qso-item">
+                <strong>Location:&nbsp;</strong>
+                {qso.qth}
+              </div>
+              <div className="qso-item">
+                <strong>Comments:&nbsp;</strong>
+                {qso.comments}
+              </div>
+              <button className="edit-qso-button" onClick={() => onEdit(qso)}>Edit</button>
+              <button className="delete-qso-button" onClick={() => onDelete(qso)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 };
